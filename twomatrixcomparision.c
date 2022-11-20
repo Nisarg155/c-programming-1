58654856d00300cdb03be8cda478d2498737c49f
Nisarg <amlaninisarg15@gmail.com>
2022-11-20 21:14:48 +0530

    done

--- twomatrixcomparision.c
+++ twomatrixcomparision.c
@@ -1,20 +1,17 @@
+ #include<stdio.h>
 
-#include<stdio.h>
 
 int main()
 {
-    int r,c ;
-    printf("Enter the number  of rows : ");
-   scanf("%d",&r);
-   printf("\nEnter the number of columns : ");
-   scanf("%d",&c);
-
-int mat1[r][c],mat2[r][c];
+    int n ;
+    printf("Enter the order of matrix : ");
+   scanf("%d",&n);
+int mat1[n][n],mat2[n][n];
 int i,j,l,k,m,m2 = 0;
-int rep[r*c] ,count=0;
-for(i=0;i<r;i++)
+int rep[n*n] ,count=0;
+for(i=0;i<n;i++)
 {
-    for(j=0;j<c;j++)
+    for(j=0;j<n;j++)
     {
        printf("\nEnter the elements of  matrix position %d%d :  ",i,j);
        scanf("%d",&mat1[i][j]);
@@ -22,9 +19,9 @@ for(i=0;i<r;i++)
  
 
 }
-for(i=0;i<r;i++)
+for(i=0;i<n;i++)
 {
-    for(j=0;j<c;j++)
+    for(j=0;j<n;j++)
     {
        printf("\nEnter the elements 2nd matrix of position %d%d :  ",i,j);
        scanf("%d",&mat2[i][j]);
@@ -33,36 +30,50 @@ for(i=0;i<r;i++)
 
 }
 
-for(l=0;l<r;l++)
+for(l=0;l<n;l++)
 {
-    for ( k = 0; k < c; k++)
+    for ( k = 0; k < n; k++)
     {
-        for(i=0;i<r;i++)
+        for(i=0;i<n;i++)
         {
-            for(j=0;j<c;j++)
+            for(j=0;j<n;j++)
             {
                 if(mat1[l][k] == mat2[i][j])
                 {
                     rep[count] = mat2[i][j];
-                    count++;           
+                    count++;
+                 
+                        if(count>1)
+                    {
+                        for ( m = 0; m < count-1; m++)
+                        {
+                            if(rep[count-1]!=rep[m])
+                            {
+                                m2 = 1;
+                            }
+                            else 
+                            m2 = 0;
+                            
+                        }
+                        if(m2 )
+                        {
+                            printf("The repeting values are : %d \n",rep[count-1]);
+                        }
+                        
+                        
+                    }
+                    else
+                    {
+                        printf("The repeting value is : %d \n",rep[0]);
+
+                    }
                    
                 }
-               
             }
         }
     }
 
     
 }
-
-for ( i = 0; i < count; i++)
-{
-    for ( j = 0; j < count; j++)
-    {
-        /* code */
-    }
-    
-}
-
 return 0;
 }
