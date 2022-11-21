6e7e34f9bda1664cf9c62cef77415a50492c88cb
Nisarg <amlaninisarg15@gmail.com>
2022-11-21 09:41:34 +0530

    tmc

--- twomatrixcomparision.c
+++ twomatrixcomparision.c
@@ -3,15 +3,18 @@
 
 int main()
 {
-    int n ;
-    printf("Enter the order of matrix : ");
-   scanf("%d",&n);
-int mat1[n][n],mat2[n][n];
+    int r,c ;
+    printf("Enter the rows of matrix : ");
+   scanf("%d",&r);
+
+   printf("\nEnter the column of matrrix :");
+   scanf("%d",&c);
+int mat1[r][c],mat2[r][c];
 int i,j,l,k,m,m2 = 0;
-int rep[n*n] ,count=0;
-for(i=0;i<n;i++)
+int rep[r*c] ,count=0;
+for(i=0;i<r;i++)
 {
-    for(j=0;j<n;j++)
+    for(j=0;j<c;j++)
     {
        printf("\nEnter the elements of  matrix position %d%d :  ",i,j);
        scanf("%d",&mat1[i][j]);
@@ -19,9 +22,9 @@ for(i=0;i<n;i++)
  
 
 }
-for(i=0;i<n;i++)
+for(i=0;i<r;i++)
 {
-    for(j=0;j<n;j++)
+    for(j=0;j<c;j++)
     {
        printf("\nEnter the elements 2nd matrix of position %d%d :  ",i,j);
        scanf("%d",&mat2[i][j]);
@@ -30,13 +33,13 @@ for(i=0;i<n;i++)
 
 }
 
-for(l=0;l<n;l++)
+for(l=0;l<r;l++)
 {
-    for ( k = 0; k < n; k++)
+    for ( k = 0; k < c; k++)
     {
-        for(i=0;i<n;i++)
+        for(i=0;i<r;i++)
         {
-            for(j=0;j<n;j++)
+            for(j=0;j<c;j++)
             {
                 if(mat1[l][k] == mat2[i][j])
                 {
