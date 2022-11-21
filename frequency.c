e51163dbbf7a49091522f961aacf436ae0734643
Nisarg <amlaninisarg15@gmail.com>
2022-11-22 00:08:34 +0530

    done

---
+++ frequency.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the n");
+    scanf("%d",&n);
+
+    int a[n],i,j,count = 0;
+    int c[100] = {1};
+     for ( i = 0; i < n; i++)
+     {
+        printf("\nenter %d elemene :",i+1);
+        scanf("%d",&a[i]);
+     }
+     for ( i = 0; i < n; i++)
+     {
+        count = 0;
+        for ( j = i + 1 ; j < n; j++)
+        {
+            
+            if (a[i] == a[j])
+            {
+                c[j] = 0;
+
+                count++;
+            }
+
+            
+            
+        }
+        c[i] = count ;
+        
+        
+     }
+
+     for ( i = 0; i < n; i++)
+     {
+        if (c[i] != 0 )
+        {
+            printf("The number is %d and its frequency is %d\n",a[i],c[i]);
+        }
+        
+     }
+     
+     
+
+
+}
---
+++ transposeofmatrix.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int r,c;
+    printf("enter the rows : ");
+    scanf("%d",&r);
+    printf("enter the columns : ");
+    scanf("%d",&c);
+
+    int a[r][c] ;
+
+    for (int i = 0; i < r; i++)
+    {
+        for (  int j = 0; j < c; j++)
+        {
+            printf("\nenter the element a %d%d : ",i+1,j+1);
+            scanf("%d",&a[i][j]);
+
+        }
+        
+    }
+    printf( " The transpose is :\n");
+    for (int  i = 0; i < c; i++)
+    {
+        for (int j = 0; j < r; j++)
+        {
+            printf("%d  ",a[j][i]);
+        }
+        printf("\n");
+        
+    }
+    
+    
+
+}
