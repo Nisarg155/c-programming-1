3b93b364ca8795bd98817cb2f3a85177acc3c572
Nisarg <amlaninisarg15@gmail.com>
2022-11-26 11:12:43 +0530

    done

--- meanmedianmode.c
+++ meanmedianmode.c
@@ -2,14 +2,11 @@
 
 int main()
 {
-    int i=0,j,count = 0,temp;
-    float a[100],sum=0;
+    int i=0,j,count = 0;
+    float a[100],b[100] ,sum=0,temp,max =0;
 do
-{
-    printf("enter : ");
-    scanf("%f",&a[i]);
-    
-
+{   
+    scanf("%f",&a[i]); 
      if(a[i] == -1)
     {
         break;
@@ -17,16 +14,14 @@ do
     else
     {
         sum += a[i];
+        b[i] = -1;
         count++;
     }
-    
     i++;
-    
-
 } while (1);
 
     float mean ,median;
-    mean = sum /(float)count;
+    mean = sum /count;
 
     for ( i = 0; i < count; i++)
     {
@@ -34,32 +29,65 @@ do
         {
             if (a[i] > a[j])
             {
-                temp = a[j];
-                a[j] = a[i];
+                temp = a[i] ;
                 a[i] = a[j];
-            }
-            
-        }
-        
+                a[j] = temp;
+            }   
+        }   
     }
-
     if (count%2!=0)
     {
-        i = (count +1)/2;
+        i = (count +1)/2.0 - 1;
 
-        median = a[i-1];
+        median = a[i];
     }
     else
     {
         i = count/2;
-        median = (a[i-1] + a[i])/2;
-
+        median = (a[i-1] + a[i])/2.0;
     }
+    printf("%.2f %.2f",mean,median);
 
-    printf("%.2f%.2f",mean,median);
-    
-    
-    
+    int frequency,index,rep = 0;
 
-     
+    for ( i = 0; i < count; i++)
+    {
+        frequency = 1;
+        for ( j = i+1; j < count; j++)
+        {
+            if (a[i] == a[j])
+            {
+                frequency++;     
+            }   
+        }
+        b[i] = frequency;
+
+        if (b[i] > max)
+        {
+            max = b[i];
+            index = i;
+        }    
+    }
+    for ( i = 0; i < count; i++)
+    {
+        if (b[i] == max)
+        {
+            rep++;
+        }     
+    }
+    
+    if (count>1)
+    {
+           if (rep >1)
+    {
+        printf(" -1.00");
+    }
+    else 
+    printf(" %.2f",a[index]);
+    }
+    else
+    {
+        printf(" -1.00");
+    }
+   return 0;
 }
