a970b8209d1fd961bdffb623a69b260621700a79
Nisarg <amlaninisarg15@gmail.com>
2022-12-08 23:02:40 +0530

    updated tic tac toe.c

--- arrays/tictactoe.c
+++ arrays/tictactoe.c
@@ -56,7 +56,7 @@ int main()
                 sum_f += a[x][y];
             }
         }
-        else // player 2 turn 
+        else // player 2 turn
         {
             printf("Enter the input of pl2\n");
             scanf("%d%d", &x, &y);
