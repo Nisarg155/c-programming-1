99343edb1c3800a003a82070bb8fd2af84422ce6
Nisarg <amlaninisarg15@gmail.com>
2022-12-15 23:22:25 +0530

    1

--- Hackerrank/piglatin.c
+++ Hackerrank/piglatin.c
@@ -1,18 +1,33 @@
 #include<stdio.h>
+#include<ctype.h>
+#include<string.h>
 
 
 int main() 
 {
-    char str[5][40],n = 0;
-    for (int i = 0; i < 5; i++)
+    char str[3][40], b[15];
+    int i ,count = 0;
+    for (int i = 0; i < 3; i++)
     {
-        scanf("%[^\n]s",str[i]);
-        n++;
+        gets(str[i]);
     }
-    for (int i = 0; i < n; i++)
+   
+    int ind = 0;
+    for ( i = 0; i < 3; i++)
     {
-        printf("%s",str[i]);
+        ind = 0;
+    int  k = 0;
+        for (int j = 0; j<strlen(str[i])+1; j++)
+        {if (str[i][j] != 32 && str[i][j] != '.' && str[i][j] != '\0')
+        ind++;
+        else{
+           b[k] = ind;
+            k++;
+        }
+        }
     }
+    
+    
     
     
     return 0;
