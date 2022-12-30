cc1f4b62efec698a05aec61939b15761950245f5
Nisarg <amlaninisarg15@gmail.com>
2022-12-30 17:35:43 +0530

    solved some merge conflict0

--- Hackerrank/piglatin.c
+++ Hackerrank/piglatin.c
@@ -1,33 +1,18 @@
 #include<stdio.h>
-#include<ctype.h>
-#include<string.h>
 
 
 int main() 
 {
-    char str[3][40], b[15];
-    int i ,count = 0;
-    for (int i = 0; i < 3; i++)
+    char str[5][40],n = 0;
+    for (int i = 0; i < 5; i++)
     {
-        gets(str[i]);
+        scanf("%[^\n]s",str[i]);
+        n++;
     }
-   
-    int ind = 0;
-    for ( i = 0; i < 3; i++)
+    for (int i = 0; i < n; i++)
     {
-        ind = 0;
-    int  k = 0;
-        for (int j = 0; j<strlen(str[i])+1; j++)
-        {if (str[i][j] != 32 && str[i][j] != '.' && str[i][j] != '\0')
-        ind++;
-        else{
-           b[k] = ind;
-            k++;
-        }
-        }
+        printf("%s",str[i]);
     }
-    
-    
     
     
     return 0;
