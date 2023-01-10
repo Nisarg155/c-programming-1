996015191304a403ed625c3b3249edb40aa05922
Nisarg <amlaninisarg15@gmail.com>
2023-01-10 20:15:31 +0530

    added some a.exe file

--- timepass/tmp1.c
+++ timepass/tmp1.c
@@ -1,17 +1,39 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main() 
+void rotate_array(char str[], int number_of_chars_to_rotate)
 {
-    char a[10],temp ;
-    scanf("%[^\n]s",a);
-    int n = strlen(a);
-    int count  = 0,space = 0;
-    for(int i = 0;i< n ;i++)
+    int n = number_of_chars_to_rotate, len = strlen(str);
+    // Write code here to rotate characters in the array by number_of_chars_to_rotate
+    // Use array traversing method
+    // Do not use array indexation method or pointer offset method.
+        char *p = NULL;
+    p = &str[len-n];
+    printf("%d\n",len);
+    for(int i = 0;i<n;i++,p++)
     {
-        if(a[i] == ' ')
-        space++;
-    } 
-    printf("total spaces %d \n total char %d",space,n - space);
+        printf("%c",*p);
+    }
+    p = &str[0];
+    for(int i = 0;i<len-n;i++,p++)
+    {
+        printf("%c",*p);
+    }
+        
+    
+}
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n;
+    char str[100];
+    scanf("%[^\n]s",str);
+    scanf("%d",&n);
+    rotate_array(str,n);
+    
     return 0;
 }
