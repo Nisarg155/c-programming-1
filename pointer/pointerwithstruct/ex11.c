c5c6d0a2f46646bf0a3fcecac7e8f64a76e9b212
Nisarg <amlaninisarg15@gmail.com>
2023-01-13 15:43:21 +0530

    added ex11.c and have some doubt in code

---
+++ pointer/pointerwithstruct/ex11.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+typedef struct {
+    char name[20];
+    int roll,marks;
+} student;
+void scan(student *p)
+{
+    printf("Enter name roll number and marks \n");
+    scanf("%s%d%d",p->name,&p->roll,&p->marks);
+}
+
+int main() 
+{
+    student s[2];
+    for (int i = 0; i < 2; i++)
+    {
+        scan(&s[i]);
+        printf("%s\n%d\n%d\n",s[i].name,s[i].roll,s[i].marks);
+    }
+    
+    
+
+    return 0;
+}
