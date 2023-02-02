69dbf637690d8e29b6521c4860eace250e447108
Nisarg <amlaninisarg15@gmail.com>
2023-02-02 16:29:22 +0530

    added new file not-dividible.. in codecheif folder

---
+++ codecheif/not-divisible-staters-76.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,i,j,n;
+    cin>>t;
+    for(i=0;i<t;i++){
+        cin>>n;
+        for(j=0;j<n;j++){
+            if(j%2==1){
+                cout<<"0"<<" ";
+            }
+            else cout<<"1"<<" ";
+
+        }
+        cout<<endl;
+    }
+	return 0;
+}
